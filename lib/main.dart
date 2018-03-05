d10d9d78dfb22e6388e8a0b719f6e630892ddd4d
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-03-05 23:27:21 +0530

    Used english words package to generate random words every time app launches

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 void main() => runApp(new MyApp());
 
+// As the app extends from StatelessWidget so it is itself a widget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
+      // The Scaffold widget provide a structure for app with default appBar and body of app
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
